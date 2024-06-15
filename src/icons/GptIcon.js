636f74a74e9e4ce170da16953dfa18edb4f2daad
Richard Kubina <rjkubina@gmail.com>
2024-06-15 18:00:06 -0400

    \#0f0 robot icon instead of emoji

--- src/icons/GptIcon.js
+++ src/icons/GptIcon.js
@@ -3,8 +3,11 @@ import React from "react";
 
 export default function GptIcon() {
   return (
-    <span>
-      🤖
-    </span>
-  );
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 17" width={30} >
+      <path
+        fill={'#0f0'}
+        d="M1 7h1v7H1v-1H0V8h1V7zM18 4h-1V3h-5V0h-2v3H5v1H4v1H3v10h1v1h1v1h12v-1h1v-1h1V5h-1V4Zm-2 6h-3V7h3v3Zm-4 4h-2v-1h2v1Zm-6-1h3v1H6v-1Zm0-6h3v3H6V7Zm7 7v-1h3v1h-3ZM22 8v5h-1v1h-1V7h1v1h1z"
+      />
+    </svg>
+  )
 }
