1f744dcdfa91578c7881cc612d30b5d5f880c31c
Richard Kubina <rjkubina@gmail.com>
2025-07-01 12:24:22 -0400

    code block svg from pixeliconlibrary

--- src/icons/CodeBlockIcon.js
+++ src/icons/CodeBlockIcon.js
@@ -2,9 +2,14 @@
 import React from "react";
 
 export default function GptIcon() {
+  // return  (
+  //   <i class="hn hn-code-block"></i>
+  // )
   return (
-    <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M11 15L8.5 12.5L11 10M14 10L16.5 12.5L14 15M5.5 6.5H19.5V18.5H5.5V6.5Z" stroke="#0f0" stroke-width="1.2"/>
-    </svg>
+    <svg width={24} height={24} fill="#FFF" xmlns="http://www.w3.org/2000/svg" id="Code_Block" data-name="Code Block" viewBox="0 0 24 24"><polygon points="14 10 13 10 13 8 14 8 14 7 15 7 15 6 16 6 16 5 15 5 15 4 14 4 14 3 13 3 13 1 14 1 14 2 15 2 15 3 16 3 16 4 17 4 17 5 18 5 18 6 17 6 17 7 16 7 16 8 15 8 15 9 14 9 14 10"/><polygon points="2 6 1 6 1 5 2 5 2 4 3 4 3 3 4 3 4 2 5 2 5 1 6 1 6 3 5 3 5 4 4 4 4 5 3 5 3 6 4 6 4 7 5 7 5 8 6 8 6 10 5 10 5 9 4 9 4 8 3 8 3 7 2 7 2 6"/><polygon points="8 10 7 10 7 8 8 8 8 6 9 6 9 4 10 4 10 2 11 2 11 1 12 1 12 3 11 3 11 5 10 5 10 7 9 7 9 9 8 9 8 10"/><polygon points="23 5 23 22 22 22 22 23 2 23 2 22 1 22 1 9 2 9 2 10 3 10 3 21 21 21 21 6 19 6 19 4 22 4 22 5 23 5"/></svg>
+
+    // <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //   <path d="M11 15L8.5 12.5L11 10M14 10L16.5 12.5L14 15M5.5 6.5H19.5V18.5H5.5V6.5Z" stroke="#0f0" stroke-width="1.2"/>
+    // </svg>
   )
 }
